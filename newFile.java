c59efc4d0f83e928d01aac56955a408c7af4b871
Vadlamudi Paramahamsa <Vadlamudi Paramahamsa>
2016-12-21 13:01:44 -0800

    asnfgfjg

--- newFile.java
+++ newFile.java
@@ -1,7 +1,9 @@
-public class newClass {
+public class newFile {
 	public static void main(String args[]){
 	
 	System.out.println("now on git");
+	
+	System.out.println("now on git new line");
 	}
 }
 
